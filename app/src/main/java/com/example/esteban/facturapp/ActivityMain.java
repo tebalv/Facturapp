b7c135f3ee144afa2c1c4bb226dbc2573c28da05
Esteban <steban.alv@gmail.com>
2017-11-18 20:18:19 -0300

    primer commit prueba

---
+++ app/src/main/java/com/example/esteban/facturapp/ActivityMain.java
@@ -0,0 +1,13 @@
+package com.example.esteban.facturapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
