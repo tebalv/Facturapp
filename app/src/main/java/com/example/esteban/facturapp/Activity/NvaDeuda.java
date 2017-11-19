ebf0d66c5704e2f26ede19e6a01881d21f5e78a8
JuanPabloOcampo <juanp.ocampo@hotmail.com>
2017-11-18 22:49:10 -0300

    Actividad Agregar Deuda

--- app/src/main/java/com/example/esteban/facturapp/Activity/NvaDeuda.java
+++ app/src/main/java/com/example/esteban/facturapp/Activity/NvaDeuda.java
@@ -3,6 +3,8 @@ package com.example.esteban.facturapp.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.esteban.facturapp.R;
+
 public class NvaDeuda extends AppCompatActivity {
 
     @Override
