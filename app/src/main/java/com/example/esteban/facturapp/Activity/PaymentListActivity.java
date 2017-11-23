4f89be056ec919080115c5e8a480b4a014dbd5ad
JuanPabloOcampo <juanp.ocampo@hotmail.com>
2017-11-23 16:32:32 -0300

    Y Con La Lista

---
+++ app/src/main/java/com/example/esteban/facturapp/Activity/PaymentListActivity.java
@@ -0,0 +1,13 @@
+package com.example.esteban.facturapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_list);
+    }
+}
